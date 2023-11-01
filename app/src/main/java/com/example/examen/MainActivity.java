f715f43b0ccf28835491f5dbe227dfa67608e579
GabiJero <angellopezm.mg@gmail.com>
2023-11-01 11:36:16 -0600

    Commit primero

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button productsButton = findViewById(R.id.button_view_one);
+    Button shoppingCartButton = findViewById(R.id.button_view_two);
+    Button newProductButton = findViewById(R.id.button_view_two);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProductsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        shoppingCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShoppingCartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        newProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewProductActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
